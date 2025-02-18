01d624eb26714683f5c1f0bb997f62407da73e27
Kagwi <kelvinmwas8@gmail.com>
2025-02-18 22:48:39 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -31,12 +31,12 @@ const About = () => {
             </div>
             <div className="grid grid-cols-2 gap-4">
               <img
-                src="https://github.com/Kagwi/mimagardens.github.io/blob/main/Mima%20Edited%202.PNG?raw=true"
+                src="https://github.com/Kagwi/mimagardens.github.io/blob/main/_SJS0713.jpg?raw=true"
                 alt="Mima Gardens 1"
                 className="w-full h-64 rounded-lg shadow-xl object-cover"
               />
               <img
-                src="https://github.com/Kagwi/mimagardens.github.io/blob/main/Mima%20Edited%204(c).PNG?raw=true"
+                src="https://github.com/Kagwi/mimagardens.github.io/blob/main/_SJS0718.jpg?raw=true"
                 alt="Mima Gardens 2"
                 className="w-full h-64 rounded-lg shadow-xl object-cover"
               />
