2276c048d8d43da7a0864cf5c2706ed76e8d954f
Kagwi <kelvinmwas8@gmail.com>
2025-03-10 16:18:39 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,6 +11,11 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
+      <Helmet>
+        {/* Add Mima Gardens logo as favicon */}
+        <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/Kagwi/mimagardens.github.io/main/Mima%20Gardens%20Logo.PNG" />
+        <title>Mima Gardens</title>
+      </Helmet>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
